a592638db0e0cee0c4b7d1a22b735ad80b0959a6
Juwel-771 <juwelah99@gmail.com>
2025-05-15 05:45:41 +0600

    project modified

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/public/images/logo.png" alt="Logo File" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
