b1f22ec6d8ef9c7aba38bd4d3ba3a47f786c37a9
Juwel-771 <juwelah99@gmail.com>
2025-05-16 12:58:11 +0600

    Navigation module added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import styles from '../Navigation/Navigation.module.css'
 const Navigation = () => {
     return (
-        <nav className='container'>
+        <nav className={`${styles.navigation} container`}>
             <div>
                 <img src="/public/images/logo.png" alt="Logo File" />
             </div>
