9432db6bba3fbf31034b5de297e1d67c46e97e51
Juwel-771 <juwelah99@gmail.com>
2025-05-17 11:53:05 +0600

    contact form created

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <section className='container'>
+            <div className='contact_form'></div>
+            <div className='contact_image'></div>
+        </section>
+    );
+};
+
+export default Contact;
